37c084271a29b7f8129f01051e2bab18ec9b505d
MAZ <29178226+MAZ01001@users.noreply.github.com>
2022-08-19 13:26:21 +0200

    Update DespawnDetection.cs

--- Assets/Scripts/DespawnDetection.cs
+++ Assets/Scripts/DespawnDetection.cs
@@ -2,4 +2,10 @@ using UnityEngine;
 
 public class DespawnDetection:MonoBehaviour{
     // TODO leave cube trigger = despawn
+    [SerializeField][Tooltip("When exiting this objects trigger/boundary, will delete this () object")]private GameObject exitTriggerObject;
+    private void OnCollisionExit(Collision collision){
+        if(collision.gameObject==this.exitTriggerObject){
+            Destroy(this.gameObject);
+        }
+    }
 }
