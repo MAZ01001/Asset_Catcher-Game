4eedbba84c1c84217ff147a4e4340ac921b791eb
MAZ <29178226+MAZ01001@users.noreply.github.com>
2022-09-23 08:32:17 +0200

    Update InputProvider.cs
    changed input of move
    and game manager as serialized field

--- Assets/Scripts/InputProvider.cs
+++ Assets/Scripts/InputProvider.cs
@@ -2,23 +2,36 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputProvider:MonoBehaviour{
-    public float move;
-    public bool jumpPressed;
+    //~ inspector (private)
+    [SerializeField][Tooltip("THE game manager")]private GameObject gameManager;
 
+    //~ public
+    public float move{get; private set;}
+    public bool jumpPressed{get; private set;}
+    //~ private
     private GameScript gameScript;
 
-    private void Start(){this.gameScript=FindObjectOfType<GameScript>();}
+    //~ get game script from game mangager
+    private void Start(){this.gameScript=this.gameManager.GetComponent<GameScript>();}
 
+    //~ initial cursor lock
     private void OnEnable(){
         Cursor.lockState=CursorLockMode.Locked;
         Cursor.visible=false;
     }
+    //~ final cursor unlock
     private void OnDisable(){
         Cursor.lockState=CursorLockMode.None;
         Cursor.visible=true;
     }
 
-    private void OnMove(InputValue value){this.move=value.Get<float>();}
+    //~ input system callbacks
+    private void OnMove(InputValue value){
+        Vector2 movement=value.Get<Vector2>();
+        this.move=movement.x;
+        if(movement.y>0f)this.jumpPressed=true;
+        else if(movement.y==0f)this.jumpPressed=false;
+    }
     private void OnJump(InputValue value){this.jumpPressed=value.isPressed;}
     private void OnPause(InputValue value){
         if(this.gameScript.gameOver)return;
