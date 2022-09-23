243ce0c7d45e956014b6868c18171e93784b5960
MAZ <29178226+MAZ01001@users.noreply.github.com>
2022-09-23 08:29:45 +0200

    removed despawn detection script

--- Assets/Scripts/DespawnDetection.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DespawnDetection:MonoBehaviour{
-    [SerializeField][Tooltip("A game object with a collider trigger that, when left, destroys this gameObject")]private GameObject keepAliveTrigger;
-
-    private void OnTriggerExit(Collider collider){
-        if(collider.gameObject==this.keepAliveTrigger)Destroy(this.gameObject);
-    }
-}
