ce88e46276fb62ff734204188e9e7af64fd92a91
MAZ <29178226+MAZ01001@users.noreply.github.com>
2022-09-23 08:30:34 +0200

    finished standalone spawner

--- Assets/Scripts/PrimitiveSpawner.cs
+++ Assets/Scripts/PrimitiveSpawner.cs
@@ -1,43 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PrimitiveSpawner:MonoBehaviour{
     //~ inspector (private)
+    [Header("Spawner")]
     [SerializeField][Tooltip("The primitives to spawn (in order of rarity ascending)")]private GameObject[] primitives;
-    // [SerializeField][Tooltip("The spawn rate per seconds")]private float spawnRate;
-    [SerializeField][Tooltip("The rarities (0.00 to 1.00 (%) in descanding order) for each element in the `primitives` array")]private float[] spawnRateRarities;
+    [SerializeField][Tooltip("The raritie (float from 0.00 to 1.00) for each element in the `primitives` array")]private float[] spawnRateRarities;
+    [SerializeField][Tooltip("The value (integer) when collected for each element in the `primitives` array")]private int[] spawnRaritiePoints;
+    [Header("Random spawn Area")]
     [SerializeField][Tooltip("The spawn origin as offset from this object for random spawning")]private Vector3 spawnOffset=new Vector3(0f,7.5f,0f);
     [SerializeField][Tooltip("The size in either direction from spawn origin for random spawning")]private Vector3 spawnAreaSize=new Vector3(7f,0f,0f);
-    //~ public methods
-    public int GetIndexOf(GameObject primitive){
-        // TODO
-        return System.Array.IndexOf<GameObject>(this.primitives,primitive);
-    }
-    public void StartSpawner(float spawnRate,float delay=0f){InvokeRepeating("SpawnPrimitive",delay,spawnRate);}
-    public void StopSpawner(){CancelInvoke("SpawnPrimitive");}
-    public void ChangeRate(float spawnRate){
-        this.StopSpawner();
-        this.StartSpawner(spawnRate);
-    }
+    [Header("Destroy Explosion")]
+    [SerializeField][Tooltip("The explosion game object to spawn (must destroy itself !)")]private GameObject explosionParticle;
+    //~ private
+    private HashSet<GameObject> spawnedPrimitives;
     //~ private methods
     private void Start(){
-        //~ sort array to escanding order and resize to primitives array length -1
-        System.Array.Sort<float>(this.spawnRateRarities,new System.Comparison<float>((f1,f2)=>f2.CompareTo(f1)));
-        System.Array.Resize<float>(ref this.spawnRateRarities,this.primitives.Length-1);
+        this.spawnedPrimitives=new HashSet<GameObject>();
+        //~ exceptions if arrays under the spawner header are not of equal lengths
+        if(this.spawnRateRarities.Length!=this.primitives.Length)throw new System.OverflowException($"[{this.gameObject.name} : PrimitiveSpawner] spawnRateRarities array is not the same length as primitives array");
+        if(this.spawnRaritiePoints.Length!=this.primitives.Length)throw new System.OverflowException($"[{this.gameObject.name} : PrimitiveSpawner] spawnRaritiesPoints array is not the same length as primitives array");
     }
+    [ContextMenu(itemName:"SpawnPrimitive")]
     private void SpawnPrimitive(){
         //~ get a random percentage as decimal between 0 to 1 (inclusive)
         float rarity=Random.value;
-        //~ get a random primitive based on the percentage in spawnRateRarities
-        GameObject prim=null;
-        for(int i=this.spawnRateRarities.Length-1;i>=0;i--){
-            if(rarity<=this.spawnRateRarities[i]){
-                prim=this.primitives[i+1];
-                break;
+        //~ get a random primitive based on the percentage in spawnRateRarities (rarest possible)
+        int minRarityIndex=-1;
+        float minRarity=float.PositiveInfinity;
+        for(int i=0;i<this.spawnRateRarities.Length;i++){
+            if(
+                this.spawnRateRarities[i]>=rarity
+                &&this.spawnRateRarities[i]<minRarity
+            ){
+                minRarityIndex=i;
+                minRarity=this.spawnRateRarities[i];
             }
         }
-        if(prim==null)prim=this.primitives[0];
+        if(minRarityIndex==-1)return;
+        GameObject prim=this.primitives[minRarityIndex];
         //~ create a new instance of that primitive at a random rotation and location within the calculated spawn area
-        Instantiate<GameObject>(
+        this.spawnedPrimitives.Add(Object.Instantiate<GameObject>(
             prim,
             this.transform.position
             +this.spawnOffset
@@ -48,6 +51,86 @@ public class PrimitiveSpawner:MonoBehaviour{
             ),
             Random.rotationUniform,
             this.transform
+        ));
+    }
+    private void OnTriggerExit(Collider collider){
+        //~ despawn if it is a primitve in the primitives array
+        if(this.spawnedPrimitives.Remove(collider.gameObject)){
+            // this.spawnedPrimitives.Remove(collider.gameObject);
+            Object.Destroy(collider.gameObject);
+        }
+    }
+    private void OnDrawGizmosSelected(){
+        Gizmos.color=Color.blue;
+        Gizmos.DrawLine(
+            this.transform.position,
+            this.transform.position
+            +this.spawnOffset
         );
+        Gizmos.color=Color.green;
+        Gizmos.DrawWireCube(
+            this.transform.position
+            +this.spawnOffset,
+            this.spawnAreaSize * 2f
+        );
+    }
+    //~ public methods
+    /// <summary>
+    ///     gets the amount of points set for that primitive or 0 if it is not a known primitive
+    ///     <br/>also spawns an explosion and destroys the primitive
+    /// </summary>
+    /// <returns> the amount of points set for that primitive or 0 if it is not a known primitive </returns>
+    public int CollectAndGetPoints(GameObject primitive){
+        if(!this.spawnedPrimitives.Contains(primitive))return 0;
+        //~ get index of primitives
+        int index=-1;
+        for(int i=0;i<this.primitives.Length;i++){
+            GameObject obj=this.primitives[i];
+            if(primitive.name==$"{obj.name}(Clone)"){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1)return 0;
+        //~ spawn explosion
+        GameObject explosionObj=Object.Instantiate<GameObject>(
+            this.explosionParticle,
+            primitive.transform.position,
+            Quaternion.identity,
+            this.transform
+        );
+        //~ set mesh and material of primitive for particle explosion
+        ParticleSystemRenderer explosionRenderer=explosionObj.GetComponent<ParticleSystemRenderer>();
+        explosionRenderer.enabled=true;
+        Renderer primitiveRenderer=primitive.GetComponent<Renderer>();
+        MeshFilter primitiveMeshFilter=primitive.GetComponent<MeshFilter>();
+        if(primitiveRenderer!=null)explosionRenderer.material=primitiveRenderer.sharedMaterial;
+        if(primitiveMeshFilter!=null)explosionRenderer.mesh=primitiveMeshFilter.sharedMesh;
+        // TODO rotation animation for rarer primitives ?
+        //~ despawn primitive
+        this.spawnedPrimitives.Remove(primitive);
+        Object.Destroy(primitive);
+        //~ return points
+        return this.spawnRaritiePoints[index];
+    }
+    /// <summary> starts the spawner after given delay and spawn rate </summary>
+    /// <param name="spawnRate"> the spawn rate per second </param>
+    /// <param name="delay"> a one time delay in seconds to start the spawning </param>
+    public void StartSpawner(float spawnRate,float delay=0f){InvokeRepeating("SpawnPrimitive",delay,spawnRate);}
+    /// <summary> stops the spawning of new objects </summary>
+    public void StopSpawner(){CancelInvoke("SpawnPrimitive");}
+    /// <summary> change the spawn rate </summary>
+    /// <param name="spawnRate"> the new spawn rate in seconds </param>
+    public void ChangeRate(float spawnRate){
+        this.StopSpawner();
+        this.StartSpawner(spawnRate);
+    }
+    /// <summary> destroys all currently existing primitives </summary>
+    [ContextMenu(itemName:"DestroyAllSpawned")]
+    public void DestroyAllSpawned(){
+        foreach(GameObject obj in this.spawnedPrimitives){
+            this.spawnedPrimitives.Remove(obj);
+            Object.Destroy(obj);
+        }
     }
 }
